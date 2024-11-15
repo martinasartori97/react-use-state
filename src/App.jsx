fbb8c9e1973ad45d8a018834c68afa2877bdc59d
martinasartori97 <martinasartori9@gmail.com>
2024-11-15 15:40:09 +0100

    edit js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import languages from './db.js'
 import AppHeader from './assets/components/AppHeader.jsx'
-
 import './App.css'
-import AppHeader from './assets/components/AppHeader.jsx'
 
 function App() {
 
--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -1,3 +1,5 @@
+
+
 export default function AppHeader() {
     return (
         <header>
